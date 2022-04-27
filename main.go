20029f2b0688b2d9c87fe9e36f01e6b75b17b56a
Chad Crouch <crouch0013@gmail.com>
2022-04-28 00:50:28 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main(
+	fs:= http.FileServer(http.Dir("./static")
+	http.Handle(pattern:"/", fs)
+
+	log.Printlin(v...: "Starting the application, listening on :8080...")
+	err := http.ListenAndServe(addr:":8080", handler:nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+)
