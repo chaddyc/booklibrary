0c1ecef212d323157bd004944d2e57f0827b7c68
Chad Crouch <crouch0013@gmail.com>
2022-04-28 23:03:35 +0200

    static files added

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ WORKDIR /app
 
 ADD . /app/
 
+RUN go mod tidy
+RUN go mod verify
+
 EXPOSE 8080
 
 CMD ["go", "run", "main.go"]
--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle(pattern:"/", fs)
-
-	log.Printlin(v...: "Starting the application, listening on :8080...")
-	err := http.ListenAndServe(addr:":8080", handler:nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	//fs := http.FileServer(http.Dir("./static"))
+	//http.Handle(pattern:"/", fs)
+	var router *gin.Engine
+	router = gin.Default()
+	router.Static("/", "./static")
 }
